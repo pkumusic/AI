d68d26faeccc450012936e248c4cea4d2967c08e
Music <Yuezhang_Li@intuit.com>
2016-07-27 15:08:29 -0700

    - Finished learning of linear_regression - Know Variable, Session and some other operations

--- learn_tf/README.md
+++ learn_tf/README.md
@@ -1,3 +1,4 @@
 This folder is for learning tensorflow.
 1: Intro.
 linear_regression.py
+https://www.tensorflow.org/versions/r0.9/get_started/index.html
--- learn_tf/linear_regression.py
+++ learn_tf/linear_regression.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
+# # initialize Interacive session
+
 # Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
@@ -9,12 +11,16 @@ y_data = x_data * 0.1 + 0.3
 # (We know that W should be 0.1 and b 0.3, but Tensorflow will
 # figure that out for us.)
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+# a = tf.Print(W, [W], message="This is W: ")
+isess = tf.Session()
+isess.run(tf.initialize_all_variables())
+print isess.run(W)
 b = tf.Variable(tf.zeros([1]))
 y = W * x_data + b
 
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
+optimizer = tf.train.GradientDescentOptimizer(0.5) # learning rate
 train = optimizer.minimize(loss)
 
 # Before starting, initialize the variables.  We will 'run' this first.
